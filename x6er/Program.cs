044fac1b32720386aad59f4f3604ebf69a478ce2
Gayvery <lokelindolsen@gmail.com>
2023-08-11 11:10:07 +0200

    Add project files.

---
+++ x6er/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace x6er
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input amount of dice");
+            int NumberOfDice = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Random random = new Random();
+
+            int[] diceRolls = new int[NumberOfDice];
+
+            int rollCount = 0;
+
+
+            do
+            {
+                for (int i = 0; i < NumberOfDice; i++)
+                {
+                    diceRolls[i] = random.Next(1, 7);
+                }
+
+                rollCount++;
+
+                Console.Write("Roll " + rollCount + ": ");
+                foreach (int roll in diceRolls)
+                {
+                    Console.Write(roll + " ");
+                }
+                Console.WriteLine();
+            }
+            while (!Roll6(diceRolls));
+
+            Console.WriteLine("It took " + rollCount + " to roll 6 on all dice");
+            // Console.WriteLine("You rolled a " + diceRoll1);
+
+        }
+
+
+        static bool Roll6(int[] diceRolls)
+        {
+            foreach (int roll in  diceRolls) 
+            {
+                if (roll != 6)
+                {
+                    return false;
+                }
+
+
+            }
+            return true;
+
+
+        }
+
+    }
+        
+
+
+    //public class dice
+    //{
+    //    public int NumberOfDice;
+    //}
+}
