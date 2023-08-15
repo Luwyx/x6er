a0d6146ab6aed82a01b67181861c9f0000254822
Gayvery <lokelindolsen@gmail.com>
2023-08-15 10:54:48 +0200

    Comments

--- x6er/Program.cs
+++ x6er/Program.cs
@@ -4,28 +4,32 @@
     {
         static void Main(string[] args)
         {
-
+            // Prompt the user to input the number of dice
             Console.WriteLine("Input amount of dice");
             int NumberOfDice = Convert.ToInt32(Console.ReadLine());
 
-
-
+            // Create a random number generator
             Random random = new Random();
 
+            // Create an array to store dice rolls
             int[] diceRolls = new int[NumberOfDice];
 
+            // Initialize a counter for the number of rolls
             int rollCount = 0;
 
-
+            // Use a do-while loop to roll the dice until all show 6
             do
             {
+                // Roll each die and store the result in the array
                 for (int i = 0; i < NumberOfDice; i++)
                 {
                     diceRolls[i] = random.Next(1, 7);
                 }
 
+                // Increment the roll count
                 rollCount++;
 
+                // Display the current roll count and the dice rolls
                 Console.Write("Roll " + rollCount + ": ");
                 foreach (int roll in diceRolls)
                 {
@@ -33,36 +37,23 @@
                 }
                 Console.WriteLine();
             }
-            while (!Roll6(diceRolls));
+            while (!Roll6(diceRolls)); // Continue rolling until all dice show 6
 
-            Console.WriteLine("It took " + rollCount + " to roll 6 on all dice");
-            // Console.WriteLine("You rolled a " + diceRoll1);
-
+            // Display the total number of rolls needed to get all dice to 6
+            Console.WriteLine("It took " + rollCount + " rolls to roll 6 on all dice");
         }
 
-
+        // Function to check if all dice show 6
         static bool Roll6(int[] diceRolls)
         {
-            foreach (int roll in  diceRolls) 
+            foreach (int roll in diceRolls)
             {
                 if (roll != 6)
                 {
-                    return false;
+                    return false; // If any die doesn't show 6, return false
                 }
-
-
             }
-            return true;
-
-
+            return true; // If all dice show 6, return true
         }
-
     }
-        
-
-
-    //public class dice
-    //{
-    //    public int NumberOfDice;
-    //}
 }
